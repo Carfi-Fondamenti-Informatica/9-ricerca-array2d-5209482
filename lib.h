d66f305d3c2a4af5176e91d103e2ee38ab396e3f
5209482 <91668718+5209482@users.noreply.github.com>
2021-12-11 16:44:01 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,17 @@
+#include "lib.h"
+#include <cmath>
+#include<limits>
 
+float max(int a, float numeri[]){
+  float m = -INFINITY;
+  
+  for(int i = 0; i < a; i++){
+    
+    if(numeri[i] > m){
+      m = numeri[i];
+    }
+    
+  }
+  
+  return m;
+}
